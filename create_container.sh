41601ad937fe267596bdfbd8cf987ca059f2feb1
gpokhark <gpokhark@gmail.com>
2023-04-14 20:05:51 -0400

    Create shell script to create container.

---
+++ create_container.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+docker run --network host -it \
+        -v /home/gaurav/Other_Drive/Udacity/FCND-Term1-Starter-Kit_gp/:/FCND-Term1-Starter-Kit_gp/ \
+        -v /home/gaurav/Other_Drive/Udacity/FCAFND/:/FCAFND/ \
+        -v /home/gaurav/Other_Drive/Udacity/FCND_Projects/:/FCND_Projects/ \
+        fcnd_new
