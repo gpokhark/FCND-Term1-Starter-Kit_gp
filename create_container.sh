79700f77746d0819e261906f50b3c4b197e1d654
gpokhark <gpokhark@gmail.com>
2023-04-22 18:26:51 -0400

    Added scikit to the docker env.

--- create_container.sh
+++ create_container.sh
@@ -3,4 +3,4 @@ docker run --network host -it \
         -v /home/gaurav/Other_Drive/Udacity/FCND-Term1-Starter-Kit_gp/:/FCND-Term1-Starter-Kit_gp/ \
         -v /home/gaurav/Other_Drive/Udacity/FCAFND/:/FCAFND/ \
         -v /home/gaurav/Other_Drive/Udacity/FCND_Projects/:/FCND_Projects/ \
-        fcnd_new
+        fcnd
